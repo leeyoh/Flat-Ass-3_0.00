b263bd4110499f20d7a52b791313fd72ea45d73d
John Lee <ljonghan@gmail.com>
2022-08-11 10:57:23 -0400

    init

---
+++ src/main/java/com/blackrock/flatiron/FlatironApplication.java
@@ -0,0 +1,13 @@
+package com.blackrock.flatiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatironApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatironApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackrock/flatiron/FlatironApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackrock.flatiron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatironApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
