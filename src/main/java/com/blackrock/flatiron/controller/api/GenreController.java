33351821d4726a0dfe7aacb577c85cce4ef10919
John Lee <ljonghan@gmail.com>
2022-08-11 16:15:06 -0400

    genre endpoint done

--- src/main/java/com/blackrock/flatiron/controller/api/GenreController.java
+++ src/main/java/com/blackrock/flatiron/controller/api/GenreController.java
@@ -1,15 +1,26 @@
 package com.blackrock.flatiron.controller.api;
+import com.blackrock.flatiron.dto.BookDTO;
+import com.blackrock.flatiron.service.BookService;
+import com.blackrock.flatiron.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/genre")
 
 public class GenreController {
-    @GetMapping
-    public String getTest(){
-        return "genre";
+    @Autowired
+    GenreService genreService;
+
+    @GetMapping("/{id}/books")
+    public List<BookDTO> getBooks(@PathVariable Long id){
+        return genreService.getBooks(id);
     }
+
 
 }
---
+++ src/main/java/com/blackrock/flatiron/service/GenreService.java
@@ -0,0 +1,33 @@
+package com.blackrock.flatiron.service;
+
+import com.blackrock.flatiron.dto.BookDTO;
+import com.blackrock.flatiron.model.Genre;
+import com.blackrock.flatiron.repository.AuthorRepository;
+import com.blackrock.flatiron.repository.BookRepository;
+import com.blackrock.flatiron.repository.GenreRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class GenreService {
+    @Autowired
+    GenreRepository genreRepository;
+    @Autowired
+    private ModelMapper mapper;
+
+    public List<BookDTO> getBooks(Long id){
+        Genre genre = genreRepository.getReferenceById(id);
+
+        return genre.getBookSet().stream().map(
+                book -> {
+                    BookDTO bookDTO = mapper.map(book, BookDTO.class);
+                    String author = book.getAuthor().getName();
+                    bookDTO.setAuthor(author);
+                    bookDTO.setGenres(book.getGenreSet().stream().map(g -> g.getName()).toList());
+                    return bookDTO;
+                }
+        ).toList();
+    }
+}
